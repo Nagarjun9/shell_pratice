38fef53e6d5717ad9944aea4ec16464aa9b02779
nagarjun9 <nagunaggu@gmail.com>
2024-08-17 08:25:22 +0530

    shell

---
+++ 02.variable.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+
+echo "Ramesh:: Hellow Suresh; How are you?" 
+echo "Suresh:: Hi Ramesh, I am fine. How are you?"
+echo "Ramesh:: I am fine too. how is your work?"
+echo "Suresh:: not bad, I am thinging upgrade to Devops"
