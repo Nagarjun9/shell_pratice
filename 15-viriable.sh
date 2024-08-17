67d1b251c6062b62854b28b23913390d34510980
nagarjun9 <nagunaggu@gmail.com>
2024-08-17 14:02:24 +0530

    shell

---
+++ 15-viriable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "please enter username::'
+
+read USERNAME #HAER USERNAME IS VARIABLE
+
+echo "please enter password::"
+
+echo "Username is : $USERNAME, Password is: $"PASSWORD"
