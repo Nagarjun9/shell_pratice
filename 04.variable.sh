e5c2a9797394823c1359d1d71fc3087eed8c220e
nagarjun9 <nagunaggu@gmail.com>
2024-08-17 08:35:25 +0530

    shell

---
+++ 04.variable.sh
@@ -0,0 +1,7 @@
+PERSONE1=$1 
+PERSONE2=$2
+
+echo "$PERSONE1:: Hellow $PERSONE2; How are you?" 
+echo "$PERSONE2:: Hi $PERSONE1, I am fine. How are you?"
+echo "$PERSONE1:: I am fine too. how is your work?"
+echo "$PERSONE2:: not bad, I am thinging upgrade to Devops"
