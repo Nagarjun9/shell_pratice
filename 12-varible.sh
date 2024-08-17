42d74cae372b32e1f28a2117d89dd95e8c23b63e
nagarjun9 <nagunaggu@gmail.com>
2024-08-17 12:21:53 +0530

    shell

---
+++ 12-varible.sh
@@ -0,0 +1,6 @@
+#!/bash/bash
+
+echo "Ramesh:: Hello Suresh; How are you?"
+echo "suresh:: Hi Ramesh , I am fine. How are you?'
+echo "Ramesh:: I am fine . How are you?"
+echo "suresh:: not bad. I am thinking to upgarde to devops"
