b6d60810a1505e53adde4b73555949332a5e15e0
nagarjun9 <nagunaggu@gmail.com>
2024-08-17 08:47:51 +0530

    shell

---
+++ 06.array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIE=("RRR" "DJTILLU" "MURARI")
+
+echo "First move is: ${MOVIE[0]}"
+echo "Second movie is: ${MOVIE[1]}"
+echo "Third movie is: ${MOVIE[2]}"
+echo "All movies is: ${MOVIE[@]}"
