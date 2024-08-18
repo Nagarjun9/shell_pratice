c1b43b50f9efe893c5642c7dfb7b985a7f5427e6
Nagarjun9 <nagunaggu@gmail.com>
2024-08-18 11:32:29 +0530

    shell

--- 18-loop.sh
+++ 18-loop.sh
@@ -36,7 +36,7 @@ fi
 for i in $@ 
 do 
     echo "package to install: $i"
-    dnf list installed $i $>>$LOGFILE 
+    dnf list installed $i &>>$LOGFILE 
     if [ $? -eq 0 ]
     then 
         echo -e "$i already  installed...$Y SKIPPING $N"
