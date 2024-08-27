03815bf13261109d3b11c0a572c39bf5114e7e61
Nagarjuna KETHUBOYINA <nagarjunake.in@mouritech.com>
2024-08-27 20:28:23 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -48,10 +48,5 @@ systemctl start mysqlid &>>$LOGFILE
 VALIDATE $? "starting MySQL Server" 
 
 mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE 
-if [ $? -ne 0 ]
-then 
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-else 
-    echo -e "MySQL Root password is already setup...$Y SKIPPING $N" 
-fi 
+
     
