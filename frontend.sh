fb1c9d9893c712f768e187eadb03be349b1d5c1d
Nagarjuna KETHUBOYINA <nagarjunake.in@mouritech.com>
2024-08-28 13:10:53 +0530

    shell

--- frontend.sh
+++ frontend.sh
@@ -44,7 +44,7 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code" 
 
 #check your repo path 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/shell_pratice/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf" 
 
 systemctl restart nginx &>>$LOGFILE
